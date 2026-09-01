2fce0f1e9821e819ae6c4483f8b76c2d1596db9b
whocares <whocares@open-scene.net>
2026-09-01 06:02:10 -0400

    Update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 #
 #
-wget https://github.com/whocares-openscene/MFCDiscordBot/archive/refs/heads/main.zip
-unzip main.zip
-mv MFCDiscordBot-main/* ./
+wget https://github.com/whocares-openscene/MFCDiscordBot/archive/refs/heads/testing.zip
+unzip testing.zip
+mv MFCDiscordBot-testing/* ./
 node bot.js
